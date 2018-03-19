231aba7c6a30283c426b2f78e9d479cee800f7d7
Carey Lee <CareyL96@gmail.com>
2018-03-18 17:45:39 -0700

    cleaned up code and added automatic updates

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,161 +1,153 @@
 // YOUR CODE HERE:
+var removeWhiteSpace = function(string) {
+  return string.replace(/ +/g, "");
+}
+
 var app = {
   server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+  messageHistory: new Set(),
   friends: new Set(),
-  rooms : new Set(),
-  
-  init: function(){
+  chatRooms: new Set(),
+
+  init: function() {
     app.fetch();
-    // app.refreshPage();
     app.handleUsernameClick();
     app.handleSubmit();
-    app.handleRoomNameClick();
+    app.handleRoomnameClick();
+    app.refreshPage();
   },
   
-  send: function(message){
+  send: function(message) {
     this.post(message);
   },
   
-  post: function(message){
+  post: function(message) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
-      success: function (data) {
+      success: function(data) {
         console.log('chatterbox: Message sent');
       },
-      error: function (data) {
+      error: function(data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to send message', data);
       }
     });
   },
+
+  // when creating set iterate through set forwards (most recent is first object in set)
+  // when rendering new messages render starting from oldest to newest 
   
-  fetch: function(){
+  fetch: function() {
     return $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
       type: 'GET',
       data: {"order":"-createdAt"},
       contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Message requested');
-        
-        data.results.forEach((dataObj) => {
-          app.renderMessage(dataObj);
-        });
-        
-        app.usernames = new Set();
-        data.results.forEach((dataObj) => {
-          dataObj.username = dataObj.username || 'undefined';
-          dataObj.roomname = dataObj.roomname || 'lobby';
-          app.usernames.add(dataObj.username.split(" ").join(""));
-          app.rooms.add(dataObj.roomname.split(" ").join(""));
-        });
-        [...app.rooms].forEach(room => app.renderRoom(room))
+      success: function(data) {
+        console.log('chatterbox: Messages requested');
+        let messages = data.results;
+
+        // iterate over receivedmessages
+        for (let i = messages.length - 1; i >= 0; i--) {
+          let messageObj = messages[i];
+          let roomname = _.escape(messageObj.roomname);
+
+          // if message hasn't been seen, append to page
+          if (!app.messageHistory.has(messageObj.objectId)){
+            app.messageHistory.add(messageObj.objectId);
+            app.renderMessage(messageObj);
+          }
+
+          // if room hasn't been seen, append to page
+          if (!app.chatRooms.has(roomname)) {
+            app.chatRooms.add(roomname);
+            app.renderRoom(roomname);
+          }
+        }
       },
-      error: function () {
+      error: function() {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         // console.error('chatterbox: Failed to send message', data);
       }
     });
   },
   
-  clearMessages: function(){
+  clearMessages: function() {
     $('#chats').empty();
   },
   
   renderMessage: function(message) {
-    var roomname = message.roomname || 'lobby'
-    roomname = app.removeWhiteSpace(roomname);
-    var userId = message.username || 'undefined';
-    var $username = `<span class="username" id=${userId.split(' ').join('')}>${message.username}</span>`;
-    var $message = `<span class="message">${_.escape(message.text)}</span>`;
-    var $chat = `<div class='chat ${roomname}'>@${$username}: ${$message}</div>`
-    $('#chats').append($chat);
+    // if message sender doesn't have username, set name to anonymous
+    let userID = _.escape(message.username) || 'anonymous';
+    let userIDClass = removeWhiteSpace(userID);
+    let messageText = _.escape(message.text);
+    let roomname = _.escape(message.roomname) || 'lobby';
+    let roomnameClass = removeWhiteSpace(roomname); 
+
+    let $username = `<a href="#" class="username">${userID}</a>`;
+    let $message = `<p class="message">${messageText}</p>`;
+    let $chat = `<div class='chat ${userIDClass} ${roomnameClass}'>${$username}: ${$message}</div>`;
+
+    if (app.friends.has(userIDClass)) {
+      $($chat).addClass('friend');
+    }
+
+    $('#chats').prepend($chat);
   },
   
-  renderRoom: function(roomName){
-    $('#roomSelect').append(`<option value="${roomName}">${roomName}</option>`);
+  renderRoom: function(roomname) {
+    $('#roomSelect').append(`<option value="${roomname}">${roomname}</option>`);
   },
 
   handleUsernameClick: function() {
-    $('#main').on('click','.username',function(){
-      var username = $(this).text();
-      app.friends.add(username);
-      // $(asdfasdf).addClass('friends');
-      console.log('friend added');
-      // when username is clicked, push to app.friends
-      $(`#${username}`).addClass('friends');
-      
-      // when friend is added
-        // get username
-        // add username to app friends
-        // use username to add friends class all dom nodes of friend
+    $('#main').on('click', '.username', function(event) {
+      event.preventDefault(); // stop page from jumping back up to top of page
+      let $username = _.escape($(this).text());
+      let usernameClass = removeWhiteSpace($username);
+
+      $(`.${usernameClass}`).addClass('friend');
+      app.friends.add($username);
     });
-  },
-  
-  refreshPage: function() {
-    setInterval(function (){app.fetch()}, 2000);
-    // app.fetch();
   },
   
   handleSubmit: function() {
-    $('#main').on('click','.submit',function(event){
-      event.preventDefault();
-      // console.log($('#message').val());
-      // username
-      // text
-      // room
-      var username = location.search.split('username=')[1];
-      var text = $('#message').val();
-      // var roomname = (roomselector).text();
-      var message = {
-        username: username,
-        text: text 
-      }
-      app.post(message);
-      console.log(message);
+    $('#main').on('click', '.submit', function(event) {
+      event.preventDefault(); // stop page from refreshing
+
+      let yourUsername = location.search.split('username=')[1];
+      let messageText = $('#message').val();
+      let currentRoomname = $('#roomSelect').val();
+      
+      let messageToSubmit = {
+        username: yourUsername,
+        text: messageText,
+        roomname: currentRoomname
+      };
+
+      app.post(messageToSubmit);
       $('#message').val('');
     });
   },
-  
-  handleRoomNameClick: function() {
+
+  handleRoomnameClick: function() {
     $('#roomSelect').on('change', function() {
-      var currentRoom = $(this).val();
-      console.log($(this).val());
-      // format currentRoom white space etc... (taken care of in the renderMessage)
-      // loop through chats
+      let currentRoomClass = removeWhiteSpace($(this).val());
+
       $('.chat').addClass('filtered');
-      $(`.${currentRoom}`).removeClass('filtered');
-      $('.filtered').slideToggle();
-      //
+      $(`.${currentRoomClass}`).removeClass('filtered');
     });
   },
-  removeWhiteSpace: function(string){
-    
-    return string.split(" ").join("");
+
+  refreshPage: function() {
+    setInterval(app.fetch.bind(app), 1000);
   }
 };
 
-$(document).ready(function(){
+$(document).ready(function() {
   app.init();
 });
-// $(document).ready(function(){
-//   // var user = new App();
-//   // var data = user.fetch();
-//   // var userData = data.responseJSON.results;
-  
-//   // userData.forEach((dataObj) => {
-//   //   user.renderMessage(dataObj);
-//   // });
-// });
-
-// displaying data
-
-// fetch data
-// save data into variable
-// iterate through data variable
-// render messages from variable 
